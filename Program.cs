52b13337b312aaa4daafef5581c22aa11351e5d4
Chad Allen <allence2@miamioh.edu>
2020-01-06 11:12:01 -0500

    Added a method that takes a number and puts each of the digits of that number into an array

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ namespace QuadaxProgrammingExerciseChadAllen
 {
     class Program
     {
+        /**
+         * Puts the digits from the answer into an array to be compared againsts the user's input
+         */
+        public static int[] numberToArray(int num)
+        {
+            int[] correctDigits = new int[4];
+            int count = 0;
+            while (num != 0)
+            {
+                correctDigits[count] = num % 10;
+                num /= 10;
+                count++;
+            }
+            return correctDigits;
+        }
+
         static int Main(string[] args)
         {
             int answer = 0, count = 10;
@@ -18,6 +34,9 @@ namespace QuadaxProgrammingExerciseChadAllen
             builder.Append(rnd.Next(1, 7));
             builder.Append(rnd.Next(1, 7));
             answer = Int32.Parse(builder.ToString());
+
+            // Calls function to put the correct answer into an array
+            int[] correctDigits = numberToArray(answer);
 
 
             // User inputs a number until they guess the correct answer
