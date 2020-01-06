db6fa03a48472aa8ad7c63bbae31d0325cbe065a
Chad Allen <allence2@miamioh.edu>
2020-01-06 12:06:19 -0500

    A final comments and minor changes for the final commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Text;
 
+/**
+ * Author: Chad Allen
+ */
 namespace QuadaxProgrammingExerciseChadAllen
 {
     class Program
     {
+        /**
+         * Puts the digits from the answer into an array to be compared againsts the user's input
+         */
+        private static int[] numberToArray(int num)
+        {
+            int[] correctDigits = new int[4];
+            int count = 3;
+            while (num != 0)
+            {
+                correctDigits[count] = num % 10;
+                num /= 10;
+                count--;
+            }
+            return correctDigits;
+        }
+
+        /**
+         * Creates a string based on how closely correct the user input is to the answer
+         */
+        private static string compareDigitArrays(int[] correct, int[] user)
+        {
+            StringBuilder correction = new StringBuilder();
+            for (int i = 0; i < user.Length; i++)
+            {
+                if (user[i] == correct[i])
+                {
+                    correction.Append("+");
+                }
+                else if (Array.IndexOf(correct, user[i]) > -1)
+                {
+                    correction.Append("-");
+                }
+                else
+                {
+                    correction.Append(" ");
+                }
+            }
+            return correction.ToString();
+        }
+
+        /**
+         * Main method
+         */
         static int Main(string[] args)
         {
             int answer = 0, count = 10;
@@ -19,21 +65,40 @@ namespace QuadaxProgrammingExerciseChadAllen
             builder.Append(rnd.Next(1, 7));
             answer = Int32.Parse(builder.ToString());
 
+            // Calls function to put the correct answer into an array
+            int[] correctDigits = numberToArray(answer);
 
-            // User inputs a number until they guess the correct answer
+            // User inputs a number until they guess the correct answer or have 10 attempts
             while (count > 0)
             {
-                Console.WriteLine("Enter a Number: (Tries left = " + count + ")");
+                Console.WriteLine("Enter a 4 digit number with digits being 1-6: (Tries left = " + count + ")");
                 int userInput;
 
                 // Check if user correctly entered a 4 digit number
-                if(int.TryParse(Console.ReadLine(), out userInput) && userInput > 999 && userInput < 10000)
+                if (int.TryParse(Console.ReadLine(), out userInput) && userInput > 999 && userInput < 10000)
                 {
+                    count--;
+                    int[] userDigits = numberToArray(userInput);
+                    string corrections = compareDigitArrays(correctDigits, userDigits);
                     
+                    // If the input from the user equals the answer the user wins 
+                    if (corrections.Equals("++++"))
+                    {
+                        Console.WriteLine("Correct!! The answer is " + userInput + " and was found in " + (10 - count) + " tries");
+                        break;
+                    }
+                    Console.WriteLine("Incorrect answer = (" + corrections + ")");
                 }
-                Console.WriteLine("Please enter a 4 digit number");
+                else
+                {
+                    Console.WriteLine("Please enter a 4 digit number");
+                }
             }
-    
+            // Loss message if user did not guess the correct answer in 10 attempts
+            if (count == 0)
+            {
+                Console.WriteLine("Failure!! The correct answer was " + answer);
+            }  
             return 0;
         }
     }
